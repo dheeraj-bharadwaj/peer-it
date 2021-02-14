7b89e0e04856d16ef93360856c0308c52ca69abf
dheeraj-bharadwaj <bharadwaj.1@iitj.ac.in>
2021-02-14 11:22:49 +0530

    made UI changes

--- public/js/main.js
+++ public/js/main.js
@@ -1,52 +1,53 @@
-const socket = io("/");
-const videoGrid = document.getElementById("videoGrid");
-const myVideo = document.createElement("video");
-myVideo.muted = true;
 
-var peer = new Peer();
+const socket = io('/')
+const videoGrid = document.getElementById('videoGrid')
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+var peer = new Peer()
 
 const myPeer = new Peer(undefined, {
-  path: "/peerjs",
-  host: "/",
-  port: "5000",
-});
+	path: '/peerjs',
+	host: '/',
+	port: '5000',
+})
 
-const peers = {};
+const peers = {}
 let fileContents;
-let myVideoStream;
+let myVideoStream
 navigator.mediaDevices
-  .getUserMedia({
-    video: true,
-    audio: true,
-  })
-  .then((stream) => {
-    myVideoStream = stream;
-    addVideoStream(myVideo, stream);
+	.getUserMedia({
+		video: true,
+		audio: true,
+	})
+	.then((stream) => {
+		myVideoStream = stream
+		addVideoStream(myVideo, stream)
 
-    socket.on("user-connected", (userName, userId) => {
-      connectToNewUser(userId, stream);
-      alert(`${userName} connected`, userId);
-    });
+		socket.on('user-connected', (userName, userId) => {
+			connectToNewUser(userId, stream);
+			alert(`${userName} connected`, userId)
+		})
 
-    peer.on("call", (call) => {
-      call.answer(stream);
-      const video = document.createElement("video");
-      call.on("stream", (userVideoStream) => {
-        addVideoStream(video, userVideoStream);
-      });
-    });
+		peer.on('call', (call) => {
+			call.answer(stream)
+			const video = document.createElement('video')
+			call.on('stream', (userVideoStream) => {
+				addVideoStream(video, userVideoStream)
+			})
+		})
 
-    let text = $("input");
+		let text = $('input')
 
-    $("html").keydown(function (e) {
-      if (e.which == 13 && text.val().length !== 0) {
-        socket.emit("message", text.val());
-        text.val("");
-      }
-    });
+		$('html').keydown(function (e) {
+			if (e.which == 13 && text.val().length !== 0) {
+				socket.emit('message', text.val())
+				text.val('')
+			}
+		})
 
-    socket.on("createMessage", (message, userName, userId) => {
-      $("ul").append(`<li >
+		socket.on('createMessage', (message, userName, userId) => {
+			$('ul').append(`<li >
 								<span class="messageHeader">
 									<span>
 										From 
@@ -55,119 +56,135 @@ navigator.mediaDevices
 										<span class="messageReceiver">Everyone:</span>
 									</span>
 
-									${new Date().toLocaleString("en-US", {
-                    hour: "numeric",
-                    minute: "numeric",
-                    hour12: true,
-                  })}
+									${new Date().toLocaleString('en-US', {
+										hour: 'numeric',
+										minute: 'numeric',
+										hour12: true,
+									})}
 								</span>
 
 								<span class="message">${message}</span>
 							
-							</li>`);
-      scrollToBottom();
-    });
+							</li>`)
+			scrollToBottom()
+		})
 
-    socket.on("fileUploaded", (fileContent, fileName, userName, userId) => {
-      this.fileContents = fileContent;
-      $("ul").append(`<li>
-				<button onclick="download()">
+		socket.on('fileUploaded', (fileContent, fileName, userName, userId) => {
+			this.fileContents = fileContent;
+			$('ul').append(`<li>
+
+			<span class="messageHeader">
+									<span>
+										From 
+										<span class="messageSender">${userName}</span> 
+										to 
+										<span class="messageReceiver">Everyone:</span>
+									</span>
+
+									${new Date().toLocaleString('en-US', {
+										hour: 'numeric',
+										minute: 'numeric',
+										hour12: true,
+									})}
+								</span>
+								<div style="padding-top:5px">
+				<button style="padding: 1px 3px 1px 3px;font-size:12px" onclick="download()">
 				${fileName}
-				</button>
-			</li>`);
-      scrollToBottom();
-    });
-  });
+				<span class="glyphicon glyphicon-circle-arrow-down"></span>
+				</button> </dov>
+			</li>`)
+			scrollToBottom()
+		})
+	})
 
-socket.on("user-disconnected", (userId) => {
-  if (peers[userId]) peers[userId].close();
-});
+socket.on('user-disconnected', (userId) => {
+	if (peers[userId]) peers[userId].close()
+})
 
-peer.on("open", (id) => {
-  socket.emit("join-room", ROOM_ID, USER_NAME, id);
-});
+peer.on('open', (id) => {
+	socket.emit('join-room', ROOM_ID, USER_NAME, id);
+})
 
 const connectToNewUser = (userId, stream) => {
-  const call = peer.call(userId, stream);
-  const video = document.createElement("video");
-  call.on("stream", (userVideoStream) => {
-    addVideoStream(video, userVideoStream);
-  });
-  call.on("close", () => {
-    video.remove();
-  });
+	const call = peer.call(userId, stream)
+	const video = document.createElement('video')
+	call.on('stream', (userVideoStream) => {
+		addVideoStream(video, userVideoStream)
+	})
+	call.on('close', () => {
+		video.remove()
+	})
 
-  peers[userId] = call;
-};
+	peers[userId] = call
+}
 
 const addVideoStream = (video, stream) => {
-  video.srcObject = stream;
-  video.addEventListener("loadedmetadata", () => {
-    video.play();
-  });
-  videoGrid.append(video);
-};
+	video.srcObject = stream
+	video.addEventListener('loadedmetadata', () => {
+		video.play()
+	})
+	videoGrid.append(video)
+}
 
 const scrollToBottom = () => {
-  var d = $(".mainChatWindow");
-  d.scrollTop(d.prop("scrollHeight"));
-};
+	var d = $('.mainChatWindow')
+	d.scrollTop(d.prop('scrollHeight'))
+}
 
 const muteUnmute = () => {
-  const enabled = myVideoStream.getAudioTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getAudioTracks()[0].enabled = false;
-    setUnmuteButton();
-  } else {
-    setMuteButton();
-    myVideoStream.getAudioTracks()[0].enabled = true;
-  }
-};
+	const enabled = myVideoStream.getAudioTracks()[0].enabled
+	if (enabled) {
+		myVideoStream.getAudioTracks()[0].enabled = false
+		setUnmuteButton()
+	} else {
+		setMuteButton()
+		myVideoStream.getAudioTracks()[0].enabled = true
+	}
+}
 
 const setMuteButton = () => {
-  const html = `
-	  <i class="fas fa-microphone"></i>
-	  <span>Mute</span>
-	`;
-  document.querySelector(".mainMuteButton").innerHTML = html;
-};
+	const html = `
+	  <i class="fas fa-microphone"></i> &nbsp;
+	  <span style="color:#fff;font-size: 15px;">Mute</span>
+	`
+	document.querySelector('.mainMuteButton').innerHTML = html
+}
 
 const setUnmuteButton = () => {
-  const html = `
-	  <i class="unmute fas fa-microphone-slash"></i>
-	  <span>Unmute</span>
-	`;
-  document.querySelector(".mainMuteButton").innerHTML = html;
-};
+	const html = `
+	  <i style="color:#fff" class="unmute fas fa-microphone-slash"></i>&nbsp;
+	  <span style="color:#fff;font-size: 15px;">Unmute</span>
+	`
+	document.querySelector('.mainMuteButton').innerHTML = html
+}
 
 const playStop = () => {
-  console.log("object");
-  let enabled = myVideoStream.getVideoTracks()[0].enabled;
-  if (enabled) {
-    myVideoStream.getVideoTracks()[0].enabled = false;
-    setPlayVideo();
-  } else {
-    setStopVideo();
-    myVideoStream.getVideoTracks()[0].enabled = true;
-  }
-};
+	console.log('object')
+	let enabled = myVideoStream.getVideoTracks()[0].enabled
+	if (enabled) {
+		myVideoStream.getVideoTracks()[0].enabled = false
+		setPlayVideo()
+	} else {
+		setStopVideo()
+		myVideoStream.getVideoTracks()[0].enabled = true
+	}
+}
 
 const setStopVideo = () => {
-  const html = `
-	  <i class="fas fa-video"></i>
-	  <span>Stop Video</span>
-	`;
-  document.querySelector(".mainVideoButton").innerHTML = html;
-};
+	const html = `
+	  <i class="fas fa-video"></i> &nbsp;
+	  <span style="color:#fff;font-size:12px">Stop Video</span>
+	`
+	document.querySelector('.mainVideoButton').innerHTML = html
+}
 
 const setPlayVideo = () => {
-  const html = `
-	<i class="stop fas fa-video-slash"></i>
-	  <span>Play Video</span>
-	`;
-  document.querySelector(".mainVideoButton").innerHTML = html;
-};
-
+	const html = `
+	<i class="stop fas fa-video-slash"></i>&nbsp;
+	  <span style="color:#fff;font-size:12px">Play Video</span>
+	`
+	document.querySelector('.mainVideoButton').innerHTML = html
+}
 
 
 const uploadFile = () => {
