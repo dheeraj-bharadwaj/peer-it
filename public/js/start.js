76592d21af37adc304f93b0313d55ce71fd7112d
dheeraj-bharadwaj <bharadwaj.1@iitj.ac.in>
2021-02-15 20:08:58 +0530

    main page

--- public/js/start.js
+++ public/js/start.js
@@ -1,8 +1,9 @@
-var roomName = prompt("Enter room Name:");
+function renderPage() {
+  var roomName = document.getElementById("rname").value;
+  var userName = document.getElementById("uname").value;
 
-var userName = prompt("Enter user name:");
-
-if(roomName && userName) {
-  // change URL, append RHS value to existing route
-  window.location = `${roomName}-${userName}`;
+  if (roomName && userName) {
+    // change URL, append RHS value to existing route
+    window.location = `${roomName}-${userName}`;
+  }
 }
